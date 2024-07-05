ad95a1246683943f4c6336441cc46f6fb4b032fb
Frab83548 <87506330+Frab83548@users.noreply.github.com>
2024-07-05 15:46:06 -0300

    Add files via upload
    d

---
+++ PruebaTecnicaHK/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace PruebaTecnicaHK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPolizaId = new System.Windows.Forms.TextBox();
+            this.txtProd = new System.Windows.Forms.TextBox();
+            this.txtTipoPro = new System.Windows.Forms.TextBox();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgvAsegurados = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsegurados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Poliza";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Producto:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Estado:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tipo Producto:";
+            // 
+            // txtPolizaId
+            // 
+            this.txtPolizaId.Location = new System.Drawing.Point(67, 55);
+            this.txtPolizaId.Name = "txtPolizaId";
+            this.txtPolizaId.Size = new System.Drawing.Size(100, 22);
+            this.txtPolizaId.TabIndex = 5;
+            // 
+            // txtProd
+            // 
+            this.txtProd.Location = new System.Drawing.Point(90, 96);
+            this.txtProd.Name = "txtProd";
+            this.txtProd.Size = new System.Drawing.Size(100, 22);
+            this.txtProd.TabIndex = 6;
+            // 
+            // txtTipoPro
+            // 
+            this.txtTipoPro.Location = new System.Drawing.Point(123, 126);
+            this.txtTipoPro.Name = "txtTipoPro";
+            this.txtTipoPro.Size = new System.Drawing.Size(100, 22);
+            this.txtTipoPro.TabIndex = 7;
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(84, 158);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(100, 22);
+            this.txtEstado.TabIndex = 8;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(207, 52);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(120, 28);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(11, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(178, 23);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Lista de Asegurados:";
+            // 
+            // dgvAsegurados
+            // 
+            this.dgvAsegurados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAsegurados.Location = new System.Drawing.Point(12, 226);
+            this.dgvAsegurados.Name = "dgvAsegurados";
+            this.dgvAsegurados.RowHeadersWidth = 51;
+            this.dgvAsegurados.RowTemplate.Height = 24;
+            this.dgvAsegurados.Size = new System.Drawing.Size(557, 148);
+            this.dgvAsegurados.TabIndex = 11;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Red;
+            this.btnEliminar.Location = new System.Drawing.Point(580, 415);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.dgvAsegurados);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.txtTipoPro);
+            this.Controls.Add(this.txtProd);
+            this.Controls.Add(this.txtPolizaId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAsegurados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPolizaId;
+        private System.Windows.Forms.TextBox txtProd;
+        private System.Windows.Forms.TextBox txtTipoPro;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgvAsegurados;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ PruebaTecnicaHK/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace PruebaTecnicaHK
+{
+    public partial class Form1 : Form
+    {
+        static string connectionString = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void CargarAseguradosActivos()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = @"SELECT a.IdAsegurado, a.Dni, a.Nombre, a.FechaNacimiento, a.Estado
+                             FROM Asegurados a
+                             WHERE a.Estado = 'Activo';";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dtAsegurados = new DataTable();
+                    adapter.Fill(dtAsegurados);
+
+                    // Agregar columna de Edad
+                    dtAsegurados.Columns.Add("Edad", typeof(int));
+
+                    foreach (DataRow row in dtAsegurados.Rows)
+                    {
+                        DateTime fechaNacimiento = Convert.ToDateTime(row["FechaNacimiento"]);
+                        int edad = CalcularEdad(fechaNacimiento);
+                        row["Edad"] = edad;
+                    }
+
+                    dgvAsegurados.DataSource = dtAsegurados;
+
+                    // Configurar las columnas visibles en el DataGridView
+                    dgvAsegurados.Columns["FechaNacimiento"].Visible = false; // Ocultar la columna de Fecha de Nacimiento
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al cargar los asegurados activos: " + ex.Message);
+            }
+        }
+
+        private int CalcularEdad(DateTime fechaNacimiento)
+        {
+            DateTime fechaActual = DateTime.Today;
+            int edad = fechaActual.Year - fechaNacimiento.Year;
+
+            // Restar un año si aún no ha pasado el día del cumpleaños en el año actual
+            if (fechaNacimiento > fechaActual.AddYears(-edad))
+                edad--;
+
+            return edad;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarAseguradosActivos();
+        }
+        private void BuscarPoliza(int polizaId)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = @"SELECT p.IdPoliza, prod.Nombre AS Producto, tp.Nombre AS TipoProducto, p.Estado
+                                     FROM Poliza p
+                                     JOIN Producto prod ON p.IdProducto = prod.IdProducto
+                                     JOIN TipoProducto tp ON p.IdTipoProducto = tp.IdTipoProducto
+                                     WHERE p.IdPoliza = @IdPoliza;
+
+                                     SELECT a.IdAsegurado, a.Dni, a.Nombre, a.FechaNacimiento, a.Estado
+                                     FROM Asegurados a
+                                     WHERE a.IdPoliza = @IdPoliza AND a.Estado = 'Activo';";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@IdPoliza", polizaId);
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+
+                    if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                    {
+                        DataRow poliza = ds.Tables[0].Rows[0];
+                        txtProd.Text = poliza["Producto"].ToString();
+                        txtTipoPro.Text = poliza["TipoProducto"].ToString();
+                        txtEstado.Text = poliza["Estado"].ToString();
+
+                        dgvAsegurados.DataSource = ds.Tables[1];
+                    }
+                    else
+                    {
+                        LimpiarControles();
+                        MessageBox.Show("No se encontró ninguna póliza con el ID proporcionado.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al buscar la póliza: " + ex.Message);
+            }
+        }
+
+        private void EliminarAsegurado(int aseguradoId)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "UPDATE Asegurados SET Estado = 'Inactivo' WHERE IdAsegurado = @IdAsegurado";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@IdAsegurado", aseguradoId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+                // Refrescar la grilla
+                CargarAseguradosActivos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error al eliminar el asegurado: " + ex.Message);
+            }
+        }
+
+        private void LimpiarControles()
+        {
+            txtProd.Text = string.Empty;
+            txtTipoPro.Text = string.Empty;
+            txtEstado.Text = string.Empty;
+            dgvAsegurados.DataSource = null;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int polizaId;
+            if (int.TryParse(txtPolizaId.Text, out polizaId))
+            {
+                BuscarPoliza(polizaId);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un ID de póliza válido.");
+            }
+        }
+
+        private void btnEliminar_Click_1(object sender, EventArgs e)
+        {
+            if (dgvAsegurados.SelectedRows.Count > 0)
+            {
+                int aseguradoId = Convert.ToInt32(dgvAsegurados.SelectedRows[0].Cells["IdAsegurado"].Value);
+                EliminarAsegurado(aseguradoId);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, seleccione un asegurado para eliminar.");
+            }
+        }
+    }
+}
